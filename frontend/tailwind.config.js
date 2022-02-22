5a4ee2e0f01fd883972ccc0d206a55f5cc60a3da
MarvinGoinar <marvingoinar@gmail.com>
2022-02-22 17:13:55 +0100

    new tailwindconfig and new peer dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
+    colors:{
+      "ghostwhite":"#E8E9F3",
+      "fieryrose":"#F05365",
+      "mango":"#FABC2A",
+      "prussianblue":"#253D5B"
+    },
   },
   plugins: [],
 }
