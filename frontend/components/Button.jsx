357f1ed6141144b38366a5db8e6d522e6c0db174
J-S-Bach <sebastian-kirner@outlook.de>
2022-02-27 20:16:05 +0100

    updated Button

--- frontend/components/Button.jsx
+++ frontend/components/Button.jsx
@@ -1,6 +1,13 @@
 const Button = (props) => {
-    return <button className="px-5 py-2 rounded-full bg-ghostwhite hover:bg-mango hover:text-white ease-in duration-300" onClick={(variable) => {props.onClick(variable)}}>
-        {props.children}
+  return (
+    <button
+      className={`px-5 py-2 rounded-full bg-ghostwhite hover:bg-mango hover:text-white ease-in duration-300 ${props.className}`}
+      onClick={(e) => {
+        props.onClick(e);
+      }}
+    >
+      {props.children}
     </button>
-}
-export default Button
+  );
+};
+export default Button;
